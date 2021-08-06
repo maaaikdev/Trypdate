5ab662a3e656ce7b43ecb2b89217986c177342ed
maaaikdev <maaaik.dev@gmail.com>
2021-08-06 17:07:16 -0500

    Ajuste listado porpiedades y cierre de componente huspedes

--- src/app/shared/guests/guests.component.ts
+++ src/app/shared/guests/guests.component.ts
@@ -9,12 +9,28 @@ declare var $: any;
 })
 export class GuestsComponent implements OnInit {
 
-	constructor() { }
+	constructor() {
+		$('body').click(function() {
+			$('#handleCounter').hide();
+		});
+		$('#handleCounter').click(function(event){
+			event.stopPropagation();
+		});
+	 }
 
 	ngOnInit() {
-		$('#handleCounter').hide();
+		//$('#handleCounter').hide();		
 	}
 	openCounter(){
-		$('#handleCounter').toggleClass('d-block');
+		$('#handleCounter').show();
+		const comp = $("#handleCounter");
+		if (comp.css('display') === 'd-block') {
+			$('body').click(function() {
+				$('#handleCounter').hide();
+			});
+			$('#handleCounter').click(function(event){
+				event.stopPropagation();
+			});
+		}
 	}
 }
